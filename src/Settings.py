ca30ea07bcf666b54b528d4abbd438c0756d228b
NikitaLitvishko <55049232+NikitaLitvishko@users.noreply.github.com>
2021-10-12 13:00:59 +0300

    setting added

---
+++ src/Settings.py
@@ -0,0 +1,16 @@
+DEBUG     = True
+INTERFACE = True
+TEMPO_SEC = 0.00
+
+TRACE = {
+    "Path.BreadthFirstSearch": 0,
+    "Path.Dijkstra": 0,
+    "Board.validFencePlacings": 0,
+    "Board.isValidFencePlacing": 0,
+    "Board.validPawnMoves": 0,
+    "Board.isValidPawnMove": 0,
+    "Board.isFencePlacingBlocking": 0,
+    "Board.getFencePlacingImpactOnPaths": 0,
+    "Board.updateStoredValidActionsAfterPawnMove": 0,
+    "Board.updateStoredValidActionsAfterFencePlacing": 0
+}
